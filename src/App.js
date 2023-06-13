1661c72ff2fc0dbe94d2c0c973a461f59e7f066e
Kanchugatla Divya <dkanchugatla@AD.MSYSTECHNOLOGIES.COM>
2023-06-13 11:37:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
+import {nanoid} from "nanoid"
 
 function App() {
+  const [data,setData] = useState([])
+  const [newTitle,setNewTitle] = useState("")
+  const [newBody,setNewBody] = useState("")
+  const [show,setShow] = useState(false)
+
+  
+  const API_URL = "https://jsonplaceholder.typicode.com/posts"
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      const response = await axios.get(API_URL)
+      const results = await response.data
+      setData(results)
+    }
+    fetchPosts();
+  },[])
+  const createPost = async () => {
+    const newPost = {
+      id: data.legth+1,
+      title: newTitle,
+      body: newBody,
+      userId:nanoid()
+    }
+    const response = await axios.post(API_URL,newPost)
+    const results = await response.data
+    setData([...data,results])
+    setShow(false)
+    console.log(results)
+    setNewBody("")
+    setNewTitle("")
+  }
+  const deletePost = async (id) => {
+    const response = await axios.delete(`${API_URL}/${id}`)
+    const result = await response.data
+    console.log(result)
+    const newData = data.filter((each)=>each.id!==id)
+    setData(newData)
+  }
+  const updatePost = async (id) => {
+    const newPost = {
+      id:id,
+      title:"updated",
+      body:"changed",
+      userId:nanoid()
+    }
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`,newPost)
+    const res = await response.data
+    console.log(res)
+    const newData = data.map((each)=>{
+      return each.id === id ? newPost : each
+    })
+    setData(newData)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <div className='header text-center'>
+           <h3>Your Post</h3>
+           {!show?<button className='btn btn-primary mt-3' onClick={()=>setShow(true)}>Create Post</button>:
+           (<div><input type="text" placeholder="Enter Title Text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)}/>
+           <input type="text" placeholder="Enter Body Text" value={newBody} onChange={(e)=>setNewBody(e.target.value)}/>
+           <button onClick={createPost}>Save</button>
+           </div>)}
+        </div>
+        {data.map((post)=>{
+          return (
+           <div className='text-center border m-3 p-2 post-container' key={post.id}>
+              <p  className="border-none"><span>Title: </span>{post.title}</p>
+              <p><span>Body: </span>{post.body}</p>
+              <button onClick={()=>updatePost(post.id)}>Update Post</button>
+              <button onClick={()=>deletePost(post.id)}>Delete Post</button> 
+              
+            </div>
+          )
+        })}
+    </>
   );
 }
 
